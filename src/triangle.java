a69e1729cef3cbde1736675500c19b61fa922853
firstquarter-J <firstquarter88@gmail.com>
2021-04-08 22:00:45 +0900

    triangle

---
+++ src/triangle.java
@@ -0,0 +1,18 @@
+
+public class triangle {
+
+	public static void main(String[] args) {
+
+		int a;
+		int b;
+		
+		for (a=0; a<5; a++) {
+			for (b=5; b>a; b--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
