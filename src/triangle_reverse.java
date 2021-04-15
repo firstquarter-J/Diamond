22ad8cb0d0fc9a14cfca2b8059a87da8bb0e5e36
firstquarter-J <firstquarter88@gmail.com>
2021-04-15 13:05:04 +0900

    21-04-15 reverse triangle

---
+++ src/triangle_reverse.java
@@ -0,0 +1,17 @@
+
+public class triangle_reverse {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		
+		for ( a=0; a<5; a++) {
+			for ( b=5; b>a; b--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
